3cc024be894c2331aa864f5608cd7aa9c444252a
Ryan Thomas <ryanthomas970@gmail.com>
2017-02-06 16:25:03 -0800

    Completed 095 Cleanup

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit, EventEmitter, Output} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { Recipe } from '../recipe';
 import {RecipeService} from "../recipe.service";
 
@@ -9,20 +9,10 @@ import {RecipeService} from "../recipe.service";
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [];
 
-  @Output() recipeSelected = new EventEmitter<Recipe>();
-
   constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
     this.recipes = this.recipeService.getRecipes();
-  }
-
-  // Used to update the recipe detail. Eventually we'll do this with routing
-  // When I click on the recipe-list, this onSelected is fired which tells me that
-  // you selected this recipe here and we want to give it to the recipe-deteail
-  // component, which will be a sibling to this compoentn
-  onSelected(recipe: Recipe) {
-    this.recipeSelected.emit(recipe);
   }
 
 }
